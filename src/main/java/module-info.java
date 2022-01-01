ab9a2a9892bd2a5c86e68deb83a2b3d71d9256b8
Eriky Ryan <62263916+erikyryan@users.noreply.github.com>
2022-01-01 15:39:12 -0300

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.sudoku.javadesktopsudoku {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.sudoku.domain to javafx.fxml;
+    exports com.sudoku.domain;
+    exports com.sudoku;
+    opens com.sudoku to javafx.fxml;
+    exports com.sudoku.userinterface;
+    opens com.sudoku.userinterface to javafx.fxml;
+}
